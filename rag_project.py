7bef11d1b1cf39517e97591bbdd00873f6d4796f
Gayatri-2810 <gpdeshmukh2004@gmail.com>
2026-05-30 18:40:37 +0530

    Update rag_project.py

--- rag_project.py
+++ rag_project.py
@@ -1,1 +1,226 @@
+!pip install langchain langchain-google-genai langchain faiss-cpu langchain-text-splitters langchain-community
 
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.prompts import PromptTemplate
+
+os.environ["GEMINI_API_KEY"] = "AIzaSyBnVDPJc2RyJpzxVmdRgCTzahXa0qNYIOg"
+
+text = """ Python Tutorial
+Last Updated : 8 May, 2026
+Python is one of the most popular programming languages. It’s simple to use, packed with features and supported by a wide range of libraries and frameworks. Its clean syntax makes it beginner-friendly.
+
+A high-level language, used in data science, automation, AI, web development and more.
+Known for its readability, which means code is easier to write, understand and maintain.
+Backed by strong library support, we don’t have to build everything from scratch.
+Web-Development.webpWeb-Development.webp
+Basic Code Example
+The following is a simple program that displays the message “Hello, World!” on the screen.
+
+
+
+
+Loading Playground...
+print("Hello World!")
+
+Output
+Hello World!
+Why Learn Python?
+Requires fewer lines of code compared to other programming languages like Java.
+Provides libraries and frameworks such as Django and Flask for web development and tools like Pandas, TensorFlow and Scikit-learn for artificial intelligence, machine learning and data analysis.
+Cross-platform, works on Windows, Mac and Linux without major changes.
+Used by top tech companies like Google, Netflix and NASA.
+Many Python coding job opportunities in Software Development, Data Science and AI/ML.
+Basics
+In this section, we’ll cover the basics of Python programming, including installation, writing first program, understanding comments and working with variables, keywords and operators. Before starting to learn python we need to install python on our system.
+
+Introduction
+Applications
+Input and Output
+Variables
+Operators
+Keywords
+Data Types
+Conditional Statements
+Loops
+Functions
+In this section of Python 3 tutorial we'll explore Python function syntax, parameter handling, return values and variable scope. Along the way, we'll also introduce versatile functions like range(), map, filter and lambda functions.
+
+Functions
+Pass in Functions
+Global and Local Variables
+Recursion
+*args and **kwargs in Function
+First Class Function
+Lambda Function
+Map, Reduce and Filter Function
+Inner Function
+Decorators
+Data Structures
+Python offers versatile collections of data types, including lists, string, tuples, sets, dictionaries and arrays. In this section, we will learn about each data types in detail.
+
+Strings
+List
+Tuples
+Dictionary
+Sets
+Arrays
+List Comprehension
+Python's collections module offers essential data structures, including the following:
+
+Counters
+Heapq
+Deque
+OrderedDict
+Defaultdict
+Complete Tutorial on DSA with Python
+OOP Concepts
+In this section, we'll explore the core principles of object-oriented programming (OOP). From encapsulation to inheritance, polymorphism, abstract classes and iterators, we'll cover the essential concepts that helps you to build modular, reusable and scalable code.
+
+Python OOP
+Classes and Objects
+‘Self’ as Default Argument
+Polymorphism
+Inheritance
+Abstraction
+Encapsulation
+Iterators
+Exception Handling
+In this section, we'll explore Exception Handling explains how Python deals with unexpected errors, enabling us to write fault-tolerant code. We'll cover file handling, including reading from and writing to files.
+
+Exception Handling
+Built-in Exception
+User defined Exception
+File Handling
+In this section, we will cover file handling, including reading from and writing to files.
+
+File Handling
+Read Files
+Write/Create Files
+OS Module
+pathlib Module
+Directory Management
+Database Handling
+In this section we will learn how to access and work with MySQL and MongoDB databases
+
+MongoDB Introduction
+MySQL Introduction
+Packages or Libraries
+Python has a huge collection of packages and standard libraries that make development easier. These libraries help with a wide range of tasks and can save a lot of time by providing ready-to-use tools.
+
+Some commonly used types of libraries include:
+
+Packages
+Built-in Modules
+DSA Libraries
+GUI Libraries
+Data Science
+1. Foundational Libraries: These are the libraries that form the base for all data science work. Start here to build a strong foundation.
+
+NumPy
+Pandas
+Matplotlib
+2. Advanced Visualization and Statistical Tools: Once you’re comfortable with basic data handling and visualization, move to creating cleaner visuals and performing statistical analysis.
+
+Seaborn
+Statsmodel
+3. Machine Learning Libraries: After data manipulation and visualization, learn machine learning, starting with simpler models and moving to advanced ones.
+
+Scikit-learn
+XGBoost /LightGBM
+4. Deep Learning Frameworks: If you’re interested in AI and deep learning, these libraries will allow you to build and train neural networks.
+
+TensorFlow and Keras
+PyTorch
+Complete Tutorial on Data Science
+Web Development
+1. Core Web Frameworks (Backend Development with Python): These are the tools for building Python-based web applications.
+
+Flask
+Django
+2. Database Integration: Learn how to connect Python web frameworks to databases for storing and retrieving data.
+
+SQLite
+SQLAlchemy
+MySQL
+3. Front-End and Backend Integration: Learn how to connect Python backends with front-end technologies to create dynamic, full-stack web applications.
+
+Jinja2 (Flask)
+Django Templates
+4. API Development: Learn to build APIs (Application Programming Interfaces) for connecting your backend with front-end apps or other services.
+
+Flask-RESTful
+Django REST Framework (DRF)
+Complete Tutorial on Web Development
+Chapter-wise Practice Quiz
+Quiz helps learners test their understanding of Python concepts through short, practice important topics and improve problem-solving skills.
+
+Python Fundamentals: Basics, Input and Output, Data Types, Numbers, Boolean.
+Control Flow & Functions: Control Flow, Loops, Functions.
+Data Structures: String, List,  Tuples, Dictionary, Sets, Arrays, List Comprehension.
+Collections Module: Counters, Heapq, Deque, OrderedDict.
+Object-Oriented Programming: OOP Concepts.
+Exception & File Handling: Exception Handling, File Handling.
+Interview Questions and Practice Problems
+This section provides a collection of commonly asked Python interview questions and practice problems to help you prepare for technical interviews and improve your coding skills.
+
+Interview Questions and Answers
+Coding Problems
+This Python tutorial is updated based on latest Python 3.13.13 version.
+
+Related Articles:
+NumPy Tutorial
+Pandas Tutorial
+Matplotlib Tutorial
+Seaborn Tutorial
+MongoDB Tutorial
+Flask Tutorial
+Django Tutorial
+Scikit Learn Tutorial
+Keras Tutorial
+LightGBM Tutorial"""
+
+splitter = RecursiveCharacterTextSplitter(chunk_size = 200 , chunk_overlap = 50)
+
+docs = splitter.create_documents([text])
+
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+vectorstore = FAISS.from_documents(docs , embeddings)
+
+retriver = vectorstore.as_retriever(search_kwargs = {"k":3})
+
+def format_docs(docs):
+  return "\n\n".join(d.page_content for d in docs)
+
+prompt = PromptTemplate(
+    input_variables = ["context","question"],
+    template = """ Use only the context below to answer the question. context: {context} question : {question} Answer : """
+)
+
+llm = ChatGoogleGenerativeAI(model = "gemini-2.5-flash")
+
+def rag_answer(query):
+  r_docs = retriver.invoke(query)
+  context = format_docs(r_docs)
+  full_prompt = prompt.format(context=context , question = query)
+  response = llm.invoke(full_prompt)
+
+  return response.content
+
+question = "What are Python data structures?"
+
+answer = rag_answer(question)
+
+print("Question:", question)
+print("\nAnswer:", answer)
+
+question ="What is Python used for?"
+
+answer = rag_answer(question)
+
+print("Question:", question)
+print("\nAnswer:", answer)
